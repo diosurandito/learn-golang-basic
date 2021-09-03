9b9898386aae6354122370cd551876b8538c0ca3
diosurandito <diosurandito@gmail.com>
2021-09-03 10:04:27 +0700

    05. Tipe Data String

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Dio")
+	fmt.Println("Dio Surandito")
+	fmt.Println("Dio Surandito Keren")
+
+	fmt.Println(len("Dio"))
+	fmt.Println("Dio Surandito"[0])
+	fmt.Println("Dio Surandito Keren"[1])
+}
