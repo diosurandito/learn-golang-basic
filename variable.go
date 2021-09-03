8e2f9ef760a77deda21216157a68e891247996f9
diosurandito <diosurandito@gmail.com>
2021-09-03 10:39:55 +0700

    06. Variable

---
+++ variable.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Dio Surandito"
+	fmt.Println(name)
+
+	name = "Surandito"
+	fmt.Println(name)
+
+	var friendName = "Bayu"
+	fmt.Println(friendName)
+
+	var age = 24
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	country = "Korea"
+	fmt.Println(country)
+
+	var (
+		firstName = "Dio"
+		lastName  = "Surandito"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
