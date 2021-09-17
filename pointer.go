d3374953732fbe3ae96b463dcdd1f84c2c616d50
diosurandito <diosurandito@gmail.com>
2021-09-17 09:09:17 +0700

    41. Pointer di Function

--- pointer.go
+++ pointer.go
@@ -6,6 +6,11 @@ type Address struct {
 	City, Province, Country string
 }
 
+// Pointer on function
+func ChangeCountryToIndonesia(address *Address) {
+	address.Country = "Indonesia"
+}
+
 func main() {
 	address1 := Address{"Brebes", "Jawa Tengah", "Indonesia"}
 	address2 := &address1
@@ -24,5 +29,18 @@ func main() {
 
 	fmt.Println(address3)
 	fmt.Println(address4)
+
+	// pointer on function
+	var alamat = Address{
+		City:     "Brebes",
+		Province: "Jawa Tengah",
+		Country:  "",
+	}
+
+	// var alamatPointer *Address = &alamat
+
+	ChangeCountryToIndonesia(&alamat)
+	// ChangeCountryToIndonesia(alamatPointer)
+	fmt.Println(alamat)
 
 }
