eaa3a6977ecb02dc7d30d5305d8d8372179608bf
diosurandito <diosurandito@gmail.com>
2021-09-23 15:08:40 +0700

    48. Package flag

---
+++ flag.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var host *string = flag.String("host", "localhost", "Put your database host")
+	var username *string = flag.String("username", "root", "Put your database username")
+	var password *string = flag.String("password", "root", "Put your database password")
+	flag.Parse()
+
+	fmt.Println("Host :", *host)
+	fmt.Println("Username :", *username)
+	fmt.Println("Password :", *password)
+}
