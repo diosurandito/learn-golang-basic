b7dd34a38187eea4af0f19b11a292aa8c31f69e3
diosurandito <diosurandito@gmail.com>
2021-09-29 14:20:18 +0700

    51. Package math

---
+++ math.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Ceil(1.40))
+	fmt.Println(math.Floor(1.60))
+	fmt.Println(math.Round(1.30))
+	fmt.Println(math.Round(1.70))
+	fmt.Println(math.Max(1, 2))
+	fmt.Println(math.Min(1, 2))
+}
